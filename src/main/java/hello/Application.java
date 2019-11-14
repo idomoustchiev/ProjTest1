4336315c67baef21e31256f052ef13021b0d044f
idomoustchiev <idomoustchiev@gmail.com>
2019-11-14 15:11:30 +0200

    Add logging to file via log4j appender

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,6 +2,8 @@ package hello;
 
 import  java.util.Arrays;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,7 +12,7 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
-
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
 	/**
 	 * SpringApplication Class can be used to bootstrap and launch a Spring application from a Java main method. 
 	 * By default class will perform the following steps to bootstrap yourapplication: 
@@ -28,13 +30,14 @@ public class Application {
         return args -> {
 
             System.out.println("Let's inspect the beans provided by Spring Boot:");
-
+            log.info("----- Logger logged this---- ");	
             String[] beanNames = ctx.getBeanDefinitionNames();
             Arrays.sort(beanNames);
             for (String beanName : beanNames) {
                 System.out.println(beanName);
             }
-
+            log.warn("----- Logger logged this warning ---- ");
+            log.debug("----- Logger logged this debug ---- ");
         };
     }
 
